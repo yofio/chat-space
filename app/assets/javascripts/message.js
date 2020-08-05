bd129e2416f9cc214f883230b412eaf05ad8b0be
yofio <d.y215jump@i.softbank.jp>
2020-08-05 20:09:39 +0900

    Update message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,10 +1,7 @@
 $(function () {
-
-
   function buildHTML(message) {
     if (message.image) {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
+      var html = `<div class="message" data-message-id=${message.id}>
           <div class="message__info">
             <div class="message__info__user-name">
               ${message.user_name}
@@ -17,11 +14,10 @@ $(function () {
             ${message.content}
           </div>
           <img class="lower-massage__image" src="${message.image}">
-        </div>`
+        </div>`;
       return html;
     } else {
-      var html =
-        `<div class="message" data-message-id=${message.id}>
+      var html = `<div class="message" data-message-id=${message.id}>
       <div class="message__info">
         <div class="message__info__user-name">
           ${message.user_name}
@@ -33,29 +29,28 @@ $(function () {
       <div class="message__text">
         ${message.content}
       </div>
-    </div>`
+    </div>`;
       return html;
-    };
+    }
   }
 
-
-  $('#new_message').on('submit', function (e) {
+  $("#new_message").on("submit", function (e) {
     e.preventDefault();
     var formData = new FormData(this);
-    var url = $(this).attr('action');
+    var url = $(this).attr("action");
     $.ajax({
       url: url,
       type: "POST",
       data: formData,
-      dataType: 'json',
+      dataType: "json",
       processData: false,
-      contentType: false
+      contentType: false,
     })
       .done(function (data) {
         var html = buildHTML(data);
-        $('.messages').append(html);
-        $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
-        $('form')[0].reset();
+        $(".messages").append(html);
+        $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight });
+        $("form")[0].reset();
         $(".submit-btn").prop("disabled", false);
       })
       .fail(function () {
@@ -66,30 +61,28 @@ $(function () {
       });
   });
   var reloadMessages = function () {
-
-    var last_message_id = $('.message:last').data("message-id");
+    var last_message_id = $(".message:last").data("message-id");
     $.ajax({
       url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: { id: last_message_id }
+      type: "get",
+      dataType: "json",
+      data: { id: last_message_id },
     })
       .done(function (messages) {
         if (messages.length !== 0) {
-          var insertHTML = '';
+          var insertHTML = "";
           $.each(messages, function (i, message) {
-            insertHTML += buildHTML(message)
+            insertHTML += buildHTML(message);
           });
-          $('.messages').append(insertHTML);
-          $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight });
+          $(".messages").append(insertHTML);
+          $(".messages").animate({ scrollTop: $(".messages")[0].scrollHeight });
         }
       })
       .fail(function () {
-        alert('error');
+        alert("error");
       });
   };
   if (document.location.href.match(/\/groups\/\d+\/messages/)) {
     setInterval(reloadMessages, 7000);
   }
 });
-
